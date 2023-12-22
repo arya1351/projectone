8b315ec7af6a96838c8226e023b68dd9cecbb10e
ardut1351 <dagung34@gmail.com>
2023-12-22 13:04:15 +0700

    add new file?

--- models.py
+++ models.py
@@ -14,15 +14,15 @@ class Users(db.Model):
 class Books(db.Model):
     __tablename__ = "tblbook"
     bookid = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), index=True, unique=True)
-    picture = db.Column(db.String(150), index=True, unique=True)
+    name = db.Column(db.String(150), unique=True)
+    picture = db.Column(db.String(150))
     isbn = db.Column(db.String(255), index=True, unique=True)
-    hargasewa = db.Column(db.Integer, index=True, unique=True)
+    hargasewa = db.Column(db.Integer, unique=True)
     
 class Pesanan(db.Model):
     __tablename__ = "tblpemesanan"
     pemesananid = db.Column(db.Integer, primary_key=True)
-    namapemesan = db.Column(db.String(150), index=True, unique=True)
-    noidentitas = db.Column(db.Integer, index=True)
+    namapemesan = db.Column(db.String(150), unique=True)
+    noidentitas = db.Column(db.Integer, )
     bukusewa = db.Column(db.String(255), index=True, unique=True)
-    notelepon = db.Column(db.Integer, index=True)  
+    notelepon = db.Column(db.Integer)  
